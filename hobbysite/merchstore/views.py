dcc38ee1549d19ff80fd5c3f8fffdb57fa3271f7
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 12:12:52 +0800

    Updated index view to list all products

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    return render(request, "index.html", {
+        "products": Product.objects.all()
+    })
 
 def item(request, itemID):
     return render(request, "item.html")
