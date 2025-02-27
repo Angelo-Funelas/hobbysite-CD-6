c3b1f7286ed4372889f7c2ee61f2239512fa73ed
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:06:28 +0800

    Setup project and app paths with empty views

---
+++ hobbysite/merchstore/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path("", lambda request: redirect("index")),
+    path("items", views.index, name="index"),
+    path("item", views.item, name="item"),
+]
