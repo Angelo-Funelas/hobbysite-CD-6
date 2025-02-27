3d6d62e8d0079fe212d601ce55f2ff7eea4ad9cb
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:37:53 +0800

    Registered models to admin panel

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ProductTyepAdmin(admin.ModelAdmin):
+    model = ProductType
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+admin.site.register(ProductType, ProductTyepAdmin)
+admin.site.register(Product, ProductAdmin)
