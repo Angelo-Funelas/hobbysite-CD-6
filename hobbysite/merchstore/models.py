c5d5a4e1922313c8287bc1e0745357be48a8a41f
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:37:39 +0800

    Fixed product_type and price field errors in Product model

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -7,6 +7,6 @@ class ProductType(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, related_name="products")
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name="products")
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2)
+    price = models.DecimalField(decimal_places=2, max_digits=12)
