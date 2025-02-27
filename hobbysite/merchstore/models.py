b2010b0d7738e3f6be5370a7d32a2cdf728723d7
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:23:36 +0800

    Added models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, related_name="products")
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=2)
