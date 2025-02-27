93b956cca6fe90c4555364fd7ef5ed46a1a5ac07
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:46:50 +0800

    Added search fields, filters, and list display to admin models

--- hobbysite/merchstore/admin.py
+++ hobbysite/merchstore/admin.py
@@ -4,9 +4,13 @@ from .models import *
 # Register your models here.
 class ProductTyepAdmin(admin.ModelAdmin):
     model = ProductType
+    search_fields = ("name",)
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
+    search_fields = ("name",)
+    list_filter = ("product_type",)
+    list_display = ("name", "product_type", "price",)
 
 admin.site.register(ProductType, ProductTyepAdmin)
 admin.site.register(Product, ProductAdmin)
