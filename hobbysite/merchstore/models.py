6f74d123e6ef7c1516f293cb70e0aa8c64f62601
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 11:43:21 +0800

    Added string representations to models

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -4,9 +4,13 @@ from django.db import models
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    def __str__(self):
+        return self.name
     
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name="products")
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=12)
+    def __str__(self):
+        return self.name
