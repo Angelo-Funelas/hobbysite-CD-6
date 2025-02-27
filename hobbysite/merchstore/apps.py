e308f922a82ab49a22cf8dd7564b0726256f08a2
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-02-27 10:45:02 +0800

    Renamed merchandise_store to merchstore

--- hobbysite/merchstore/apps.py
+++ hobbysite/merchstore/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MerchandiseStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'merchandise_store'
+    name = 'merchstore'
