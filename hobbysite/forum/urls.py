556a07c8e53896f039401399057dba2a40671e71
Justin <justinbondc322@gmail.com>
2025-02-28 17:02:52 +0800

    Changed 'thread_number' to 'thread_num'.

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('/forum/threads', thread_list, name="thread_list"),
-    path('/forum/thread/<int:thread_number', detailed_thread, name="detailed_thread"),
+    path('/forum/thread/<int:thread_num', detailed_thread, name="detailed_thread"),
 ]
 
 app_name = "forum"
