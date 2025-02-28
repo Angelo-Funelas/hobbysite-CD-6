1aa7976db0c2595c086db5aced4f181abe60aa9a
Justin <justinbondc322@gmail.com>
2025-02-28 17:07:42 +0800

    Created 2 (incomplete) views: thread_list and detailed_thread.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def thread_list(request):
+    
+    threads = 
+    return render(request, 'thread_list.html', {'threads': threads})
+
+def detailed_thread(request, thread_num):
+
+    thread = 
+    return render(request, 'thread.html', {'thread': thread})
