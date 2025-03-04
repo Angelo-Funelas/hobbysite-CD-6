8dc2f90639dd87c66cf4796806bb5f0709ac57b6
Justin <justinbondc322@gmail.com>
2025-03-04 18:48:46 +0800

    Added meta classes to fix model names in admin panel.

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -5,9 +5,23 @@ class PostCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Post Category'
+        verbose_name_plural = 'Post Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True, related_name="category")
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
