4641edfbd268eed9baa70433826d1b31296945cd
Justin <justinbondc322@gmail.com>
2025-03-04 18:36:21 +0800

    Created the models and their fields according to the specifications.

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True, related_name="category")
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
