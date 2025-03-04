5d30e7205c61f638a99fbc2ffed0e06794ffee9d
Justin <justinbondc322@gmail.com>
2025-03-04 18:33:45 +0800

    Modified thread_list view to account for threads based on the category given, and modified thread variable in detailed_thread to get objects based on their id number.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from .models import *
 
-def thread_list(request):
+def thread_list(request, category_name):
     
-    threads = 
+    threads = PostCategory.objects.get(name=category_name)
     return render(request, 'thread_list.html', {'threads': threads})
 
 def detailed_thread(request, thread_num):
 
-    thread = 
+    thread = Post.objects.get(id=thread_num)
     return render(request, 'thread.html', {'thread': thread})
