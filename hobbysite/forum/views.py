6bf34c58705b6036c9e193d98dd5e166952bf375
Justin <justinbondc322@gmail.com>
2025-03-09 20:57:09 +0800

    Created view to display all threads of selected category.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -6,6 +6,11 @@ def thread_list(request):
     threads = PostCategory.objects.all()
     return render(request, 'thread_list.html', {'threads': threads})
 
+def threads_in_category(request, category_name):
+
+    threads = PostCategory.objects.get(name=category_name)
+    return render(request, 'threads_in_category.html', {'threads': threads})
+
 def detailed_thread(request, thread_num):
 
     thread = Post.objects.get(id=thread_num)
