0badf3545b33980f66852fb5c82ebe7ec9833c40
nclvrl <nicolecdverial@gmail.com>
2025-03-09 14:27:54 +0800

    Added models

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class ArticleCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name'] # Sorted by name in ascending order
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name="articles")
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_on'] # Sorted by creation date in descending order
