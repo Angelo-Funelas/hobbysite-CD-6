234bf15743f9559a657368706e6b5dd6ef8c2603
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-03-09 17:39:02 +0800

    [hobbysite urls] added path for commission links

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path("", lambda request: redirect("commission"))
-    path('commissions/list', views.commission, name = "commission"),
-    path('commissions/detail/<int:pk>/', views.commission_details, name = "commission_details"),
+    path("", lambda request: redirect("commission")),
+    path('/list', views.commission, name = "commission"),
+    path('/detail/<int:pk>/', views.commission_details, name = "commission_details"),
 ]
 
 app_name = "commissions"
