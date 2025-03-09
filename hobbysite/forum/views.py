a650f5f4701469b8de5a58317f54ce9a4a76831f
Justin <justinbondc322@gmail.com>
2025-03-09 20:50:49 +0800

    Modified thread_list view to get all PostCategory objects.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import *
 
-def thread_list(request, category_name):
+def thread_list(request):
     
-    threads = PostCategory.objects.get(name=category_name)
+    threads = PostCategory.objects.all()
     return render(request, 'thread_list.html', {'threads': threads})
 
 def detailed_thread(request, thread_num):
