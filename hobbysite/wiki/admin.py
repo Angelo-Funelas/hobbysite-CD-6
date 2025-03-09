fbbc404e823ee563d0b04c40cfa6d3fa7fb9c588
nclvrl <nicolecdverial@gmail.com>
2025-03-09 14:43:03 +0800

    Registered models to admin panel

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    list_display = ['title', 'category', 'created_on', 'updated_on']
+    list_filter = ['category']
+
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
