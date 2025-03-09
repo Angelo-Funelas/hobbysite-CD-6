b6e1f1ecb6d5475a4775a228e0aeb6c523108f49
Justin <justinbondc322@gmail.com>
2025-03-09 15:07:52 +0800

    Fixed paths.

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('forum/threads', thread_list, name="thread_list"),
-    path('forum/thread/<int:thread_num', detailed_thread, name="detailed_thread"),
+    path('threads', thread_list, name="thread_list"),
+    path('thread/<int:thread_num>', detailed_thread, name="detailed_thread"),
 ]
 
 app_name = "forum"
