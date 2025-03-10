875308adf0258b67c112a6727ed47e5b5dd6a5f0
Justin <justinbondc322@gmail.com>
2025-03-10 12:32:20 +0800

    Changed code to make it shorter.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -2,16 +2,18 @@ from django.shortcuts import render
 from .models import *
 
 def thread_list(request):
-    
-    threads = PostCategory.objects.all()
-    return render(request, 'thread_list.html', {'threads': threads})
+    return render(request, 'thread_list.html', {
+        'threads': PostCategory.objects.all()
+    })
 
 def threads_in_category(request, category_name):
-
-    threads = PostCategory.objects.get(name=category_name)
-    return render(request, 'threads_in_category.html', {'threads': threads})
+    chosen_category = PostCategory.objects.get(name=category_name)
+    return render(request, 'threads_in_category.html', {
+        'threads': Post.objects.filter(category=chosen_category),
+        'chosen_category': chosen_category
+    })
 
 def detailed_thread(request, thread_num):
-
-    thread = Post.objects.get(id=thread_num)
-    return render(request, 'thread.html', {'thread': thread})
+    return render(request, 'thread.html', {
+        'thread': Post.objects.get(id=thread_num)
+    })
