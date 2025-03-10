21b8c24523f87243bc7b3b65b094a848361bcde6
Justin <justinbondc322@gmail.com>
2025-03-10 12:31:41 +0800

    Fixed threads_in_category path.

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('threads', thread_list, name="thread_list"),
+    path('threads/<str:category_name>', threads_in_category, name="threads_in_category"),
     path('thread/<int:thread_num>', detailed_thread, name="detailed_thread"),
 ]
 
