0f597d71935bd2fb4ea94b4020408a32202dd925
nclvrl <nicolecdverial@gmail.com>
2025-03-11 19:21:38 +0800

    Modified articles_list to add ArticleCategory to context

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -3,7 +3,11 @@ from .models import *
 
 # Create your views here.
 def articles_list(request):
-    return render(request, "articles_list.html", {"articles": Article.objects.all()})
+    ctx = {
+        "articles": Article.objects.all(),
+        "categories": ArticleCategory.objects.all()
+    }
+    return render(request, "articles_list.html", ctx)
 
 def article_detail(request, pk):
     return render(request, "article.html", {"article": Article.objects.get(pk=pk)})
