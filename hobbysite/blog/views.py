4c21336c858f2e325af8519f20cf6f2297a3ce63
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-03-13 20:50:58 +0800

    Fixed issue #6 - placed templates inside blog folder for namespace.

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from .models import *
 
 def articles_list(request):
-    return render(request, "article_list.html", 
+    return render(request, "blog/article_list.html", 
                   {"articles": Article.objects.all()})
 
 def article_detail(request, article_id):
-    return render(request, "article_detail.html", {
+    return render(request, "blog/article_detail.html", {
         "article": Article.objects.get(id=article_id)
     })
 # Create your views here.
