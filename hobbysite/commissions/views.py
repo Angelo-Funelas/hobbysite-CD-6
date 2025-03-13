3d09017ecb56959deb455b8882d2cb0d395ca5f3
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-03-13 20:51:35 +0800

    Fixed issue #6 - placed templates inside forum folder for namespace.

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -3,12 +3,12 @@ from .models import *
 
 # Create your views here.
 def commission(request):
-    return render(request, "commissions.html", {
+    return render(request, 'commissions/commissions.html', {
         "commissions": Commission.objects.all()
     })
 
 def commission_details(request, pk):
-    return render(request, "commissions_detail.html", {
+    return render(request, 'commissions/commissions_detail.html', {
         "commission": Commission.objects.get(pk=pk),
         "comments": Commission.objects.get(pk=pk).comments.all()
     })
