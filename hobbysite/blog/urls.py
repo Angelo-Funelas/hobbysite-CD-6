eeec13b35344870b65b8c395d2e29ead254a7ca8
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-03-13 21:01:22 +0800

    [urls.py] Removed trailing "/" in urls.py
    Signed-off-by: JPChurros <151139142+JPChurros@users.noreply.github.com>

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("articles/", articles_list, name="index"),
-    path("article/<int:article_id>/", article_detail, name="item"),
+    path("articles", articles_list, name="index"),
+    path("article/<int:article_id>", article_detail, name="item"),
 ]
 
 app_name = "blog"
