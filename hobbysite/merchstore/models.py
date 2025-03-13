fbdc5f915e48b50cde3ba1c56e6852145e032e6f
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-03-13 20:48:51 +0800

    Added spacing for model methods

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -5,8 +5,10 @@ from django.urls import reverse
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    
     def __str__(self):
         return self.name
+    
     class Meta:
         ordering = ["name"] # order by name ascending order
     
@@ -15,9 +17,12 @@ class Product(models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True, related_name="products")
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=12)
+    
     def __str__(self):
         return self.name
+    
     def get_absolute_url(self):
         return reverse("merchstore:item", kwargs={"id": self.id})
+    
     class Meta:
         ordering = ["name"] # order by name ascending order
