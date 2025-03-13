a116f8f02a11b664df4e0933df7bb982f1bdaa3b
Marcus Jet Esteban <marcusjetesteban@Marcuss-MacBook-Pro.local>
2025-03-13 21:31:56 +0800

    fixed coding conventions

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -4,8 +4,8 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path("", lambda request: redirect("commissions:commission")),
-    path('list', commission, name = "commission"),
-    path('detail/<int:pk>', commission_details, name = "commission_details"),
+    path('list', commission, name="commission"),
+    path('detail/<int:pk>', commission_details, name="commission_details"),
 ]
 
 app_name = "commissions"
