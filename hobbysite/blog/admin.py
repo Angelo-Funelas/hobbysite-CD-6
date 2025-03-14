0b641b5d91c193a267590e2145ce58d44f8b4ad3
JPChurros <151139142+JPChurros@users.noreply.github.com>
2025-03-14 19:32:41 +0800

    [admin.py] Fixed formatting
    Signed-off-by: JPChurros <151139142+JPChurros@users.noreply.github.com>

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -3,14 +3,14 @@ from .models import *
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
-    list_display = ['name',]
-    search_display = ['name',]
+    list_display = ('name',)
+    search_display = ('name',)
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
-    list_display = ['title', 'category', 'created_on', 'updated_on',]
-    list_filter = ['category', 'created_on']
-    search_fields = ['title', 'content',]
+    list_display = ('title', 'category', 'created_on', 'updated_on',)
+    list_filter = ('category', 'created_on',)
+    search_fields = ('title', 'content',)
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
