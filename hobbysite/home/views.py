725ea33e71eba250db7c9f21914a2a85ffdad57d
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-28 03:42:00 +0800

    Setup index view and template

--- hobbysite/home/views.py
+++ hobbysite/home/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    services = [
+    {
+        "name": "Wiki",
+        "url": "wiki:articles_list"
+    },
+    {
+        "name": "Commissions",
+        "url": "commissions:commission"
+    },
+    {
+        "name": "Forum",
+        "url": "forum:thread_list"
+    },
+    {
+        "name": "Blog",
+        "url": "blog:index"
+    },
+    {
+        "name": "Merchandise Store",
+        "url": "merchstore:index"
+    },
+]
+    return render(request, "home/index.html", {
+        'services': services,
+    })
