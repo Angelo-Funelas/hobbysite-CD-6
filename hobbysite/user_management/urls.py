a059c2de7ab78e7f2b4dc5f90269cd7821684e5b
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-28 01:43:27 +0800

    Fixed logout url

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", index, name="index"),
     path("register/", register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="user_management/login.html"), name="login"),
-    path("login/", auth_views.LogoutView.as_view(), name="logout"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="user_management/password_reset_form.html"), name="password_reset"),
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="user_management/password_reset_done.html"), name="password_reset_done"),
     path("password_reset/confirm/", auth_views.PasswordResetConfirmView.as_view(template_name="user_management/password_reset_confirm.html"), name="password_reset_confirm"),
