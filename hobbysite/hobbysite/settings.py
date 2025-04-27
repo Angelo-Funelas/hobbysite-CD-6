9b4421041f2b68edab60f5e3ced414f3427bfb35
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-28 02:58:43 +0800

    Added static directory in base directory as static files

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # This points to the static directory at the root of your project
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
