4d09f1415ad00bf97d9b4503bcea3b69b1a1d734
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-28 01:17:33 +0800

    Setup urls.py and views.py

---
+++ hobbysite/user_management/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import index, register
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("register/", register, name="register"),
+    path("login/", auth_views.LoginView.as_view(template_name="user_management/login.html"), name="login"),
+    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="user_management/password_reset_form.html"), name="password_reset"),
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="user_management/password_reset_done.html"), name="password_reset_done"),
+    path("password_reset/confirm/", auth_views.PasswordResetConfirmView.as_view(template_name="user_management/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("password_reset/complete/", auth_views.PasswordResetCompleteView.as_view(template_name="user_management/password_reset_complete.html"), name="password_reset_complete"),
+]
--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+@login_required
+def index(request):
+    if request.method == 'POST':
+        display_name = request.POST['display-name']
+        email = request.POST['email']
+        profile = request.user.profile
+        profile.display_name = display_name
+        profile.email_field = email
+        profile.save()
+    else:
+        return render(request, "user_management/profile.html", {
+            "profile": request.user.profile
+        })
+        
+def register(request):
+    if request.method == 'GET':
+        return render(request, "registration/register.html")
+    elif request.method == 'POST':
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        try: 
+            User.objects.get(username=username.lower())
+        except User.DoesNotExist:
+            try:
+                pfp = request.FILES["pfp"]
+                User.objects.create_user(username.lower(), email, password, pfp=pfp, display_name=username)
+            except:
+                user = User.objects.create_user(username.lower(), email, password, display_name=username) 
+            return HttpResponseRedirect(reverse('index'))
