e7680e995281924f0c881b082f420d056e776d82
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-04-28 03:07:08 +0800

    Fixed password reset confirm url

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name="user_management/password_reset_form.html"), name="password_reset"),
     path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="user_management/password_reset_done.html"), name="password_reset_done"),
-    path("password_reset/confirm/", auth_views.PasswordResetConfirmView.as_view(template_name="user_management/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="user_management/password_reset_confirm.html"), name="password_reset_confirm"),
     path("password_reset/complete/", auth_views.PasswordResetCompleteView.as_view(template_name="user_management/password_reset_complete.html"), name="password_reset_complete"),
 ]
