43e92e15abd4ab783d7e8ada3022f85f287830a4
Justin <justinbondc322@gmail.com>
2025-05-03 12:12:54 +0800

    Created Thread List View.

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,19 +1,36 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from .models import PostCategory, Post
+from .models import ThreadCategory, Thread
 
+@login_required
 def thread_list(request):
+    user_threads = Thread.objects.filter(author=request.user.profile)
+    other_threads = Thread.objects.exclude(author=request.user.profile)
+    
+    # Iterates through all categories and then groups them into respective dictionaries
+    categories = ThreadCategory.objects.all()
+    grouped_threads = []
+    for category in categories:
+        threads_in_category = other_threads.filter(category=category)
+        if threads_in_category.exists():
+            grouped_threads.append({
+                'category': category,
+                'threads': threads_in_category
+            })
+
     return render(request, 'forum/thread_list.html', {
-        'threads': PostCategory.objects.all()
+        'user_threads': user_threads,
+        'other_threads': grouped_threads
     })
 
-def threads_in_category(request, category_name):
-    chosen_category = PostCategory.objects.get(name=category_name)
-    return render(request, 'forum/threads_in_category.html', {
-        'threads': Post.objects.filter(category=chosen_category),
-        'chosen_category': chosen_category
-    })
+# def threads_in_category(request, category_name):
+#     chosen_category = ThreadCategory.objects.get(name=category_name)
+#     return render(request, 'forum/threads_in_category.html', {
+#         'threads': Thread.objects.filter(category=chosen_category),
+#         'chosen_category': chosen_category
+#     })
 
 def detailed_thread(request, thread_num):
     return render(request, 'forum/thread.html', {
-        'thread': Post.objects.get(id=thread_num)
+        'thread': Thread.objects.get(id=thread_num)
     })
