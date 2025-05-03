306d306147e88e72ee44be73aed0eddd2f589ccb
Justin <justinbondc322@gmail.com>
2025-05-03 12:10:43 +0800

    Modified admin names.

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,24 +1,24 @@
 from django.contrib import admin
-from .models import Post, PostCategory
+from .models import Thread, ThreadCategory
 
-class PostInLine(admin.StackedInline):
-    model = Post
+class ThreadInLine(admin.StackedInline):
+    model = Thread
 
-class PostCategoryAdmin(admin.ModelAdmin):
-    model = PostCategory
+class ThreadCategoryAdmin(admin.ModelAdmin):
+    model = ThreadCategory
 
     list_display = ('name', 'description',)
     search_fields = ('name', )
 
-    # Allows for admin to view Posts under a PostCategory.
-    inlines = [PostInLine, ]
+    # Allows for admin to view Threads under a ThreadCategory.
+    inlines = [ThreadInLine, ]
 
-class PostAdmin(admin.ModelAdmin):
-    model = Post
+class ThreadAdmin(admin.ModelAdmin):
+    model = Thread
 
     list_display = ('title', 'category', 'entry', 'created_on', 'updated_on', 'id', )
     search_fields = ('title', 'entry',)
     list_filter = ('created_on',)
 
-admin.site.register(PostCategory, PostCategoryAdmin)
-admin.site.register(Post, PostAdmin)
+admin.site.register(ThreadCategory, ThreadCategoryAdmin)
+admin.site.register(Thread, ThreadAdmin)
