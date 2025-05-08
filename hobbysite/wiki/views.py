e6e05486cbd7429674b33296332a3acea0be43f1
nclvrl <nicolecdverial@gmail.com>
2025-05-08 20:02:34 +0800

    Completed majority of view specifications
    - Updated articles_list view according to specs
    - Created article_create and article_update views
    - article_detail view still needs modification (to be done in future)

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
-from .models import ArticleCategory, Article
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .models import ArticleCategory, Article, Comment
+from .forms import CommentForm, NewArticleForm, UpdateArticleForm
 
 def articles_list(request):
     return render(request, 'wiki/articles_list.html', {
-        'articles': Article.objects.all(),
+        'user_articles': Article.objects.filter(author=request.user.profile),
+        'all_articles': Article.objects.exclude(author=request.user.profile),
         'categories': ArticleCategory.objects.all()
     })
 
-def article_detail(request, pk):
+def article_detail(request, pk): # Unfinished, will work on specifications later
     return render(request, 'wiki/article.html', {
         'article': Article.objects.get(pk=pk)
+    })
+
+@login_required
+def article_create(request):
+    if request.method == 'POST':
+        article_form = NewArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article = article_form.save(commit=False)
+            article.author = request.user.profile
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        article_form = NewArticleForm()
+
+    return render(request, 'wiki/create_article.html', {
+        'article_form': article_form
+    })
+
+@login_required
+def article_update(request, pk):
+    article = Article.objects.get(pk=pk)
+    if article.author != request.user.profile:
+        return redirect('article_detail', pk=pk)
+    
+    if request.method == 'POST':
+        article_form = UpdateArticleForm(request.POST, request.FILES, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect('article_detail', pk=pk)
+    else:
+        article_form = UpdateArticleForm(instance=article)
+
+    return render(request, 'wiki/update_article.html', {
+        'article': article,
+        'article_form': article_form
     })
