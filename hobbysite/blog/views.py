a5883071935d421d100665425f0f2389587e83c7
JPChurros <charlesmatthewong2019@gmail.com>
2025-05-10 23:09:17 +0800

    [templates] Fixed article_details to accomdiate new css and data [views.py] Created a rough working draft for article_details [forms.py] Created Comment and update forms for article details based on user

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .models import Article, ArticleCategory, Comment
+from .models import Article, Comment
 from user_management.models import Profile
+from .forms import CommentForm
 
 def articles_list(request):
     category_grouping = {}
@@ -34,6 +35,39 @@ def articles_list(request):
     return render(request, 'blog/article_list.html', context)
 
 def article_detail(request, article_id):
-    return render(request, 'blog/article_detail.html', {
-        'article': Article.objects.get(id=article_id)
+    try:
+        article = Article.objects.select_related('author').get(id=article_id)
+    except Article.DoesNotExist:
+        article = None
+    
+    comments = []
+    related_articles = []
+    form = None
+
+    if article:
+        comments = article.comments.select_related('author').all().order_by('-created_on')
+        related_articles = Article.objects.filter(author=article.author).exclude(id=article.id)[:2]
+
+        if request.user.is_authenticated:
+            try:
+                user_profile = request.user.profile
+            except Profile.DoesNotExist:
+                user_profile = None
+            
+            if request.method == "POST":
+                form = CommentForm(request.POST)
+                if form.is_valid():
+                    comment = form.save(commit=False)
+                    comment.author = user_profile
+                    comment.article = article
+                    comment.save()
+                    return redirect("blog:item", article_id=article.id)
+            else:
+                form = CommentForm()
+    
+    return render(request, "blog/article_detail.html", {
+        "article": article,
+        "comments": comments,
+        "related_articles": related_articles,
+        "form": form,
     })
