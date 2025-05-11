89a6dde36f642738f72e170139231fdc331d2b0c
Justin <justinbondc322@gmail.com>
2025-05-11 17:07:06 +0800

    Removed unused code.

--- hobbysite/forum/models.py
+++ hobbysite/forum/models.py
@@ -8,11 +8,7 @@ class ThreadCategory(models.Model):
 
     def __str__(self):
         return self.name
-
-    # This function gets the latest created within the category by accessing the foreign key connected to the Post model.
-    def latest_thread(self):
-        return self.threads.order_by('-created_on')[0]
-
+    
     class Meta:
         verbose_name = 'Thread Category'
         verbose_name_plural = 'Thread Categories'
@@ -21,7 +17,7 @@ class ThreadCategory(models.Model):
 class Thread(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
-    category = models.ForeignKey(ThreadCategory, on_delete=models.SET_NULL, null=True, related_name="threads")
+    category = models.ForeignKey(ThreadCategory, on_delete=models.SET_NULL, null=True)
     entry = models.TextField()
     image = models.ImageField(upload_to='images/', blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
