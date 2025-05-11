61d0e2aa6b41fb140ddf17c3e65fab9dde3c433a
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-05-11 22:44:46 +0800

    Updated urls to follow specs

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views
+from .views import index, item, add, edit, cart, transactions
 from django.shortcuts import redirect
 
 urlpatterns = [
     path("", lambda request: redirect("merchstore:index")),
-    path("items/", views.index, name="index"),
-    path("item/<int:id>/", views.item, name="item"),
-    path("item/add/", views.add, name="add"),
-    path("item/<int:id>/edit/", views.edit, name="edit"),
-    path("item/cart", views.cart, name="cart"),
-    path("item/transactions", views.transactions, name="transactions"),
+    path("items/", index, name="index"),
+    path("item/<int:id>/", item, name="item"),
+    path("item/add/", add, name="add"),
+    path("item/<int:id>/edit/", edit, name="edit"),
+    path("item/cart/", cart, name="cart"),
+    path("item/transactions/", transactions, name="transactions"),
 ]
 
 app_name = "merchstore"
