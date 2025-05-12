d3bbd84daac7ef7a265a220d9662816a555f829e
JPChurros <charlesmatthewong2019@gmail.com>
2025-05-12 16:30:02 +0800

    [urls.py] added trailing slashes

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -4,8 +4,8 @@ from django.shortcuts import redirect
 
 urlpatterns = [
     path("", lambda request: redirect("blog:index")),
-    path("articles", article_list, name="index"),
-    path("article/<int:article_id>", article_detail, name="article_detail"),
+    path("articles/", article_list, name="index"),
+    path("article/<int:article_id>/", article_detail, name="article_detail"),
     path('article/add/', article_create, name='article_create'),
     path('article/<int:article_id>/edit/', article_update, name='article_update'),
 ]
