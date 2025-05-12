3c89797daf6359915cdc9d0b86011169d289eade
Justin <justinbondc322@gmail.com>
2025-05-12 19:09:39 +0800

    Added missing comma for last url.

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("threads/", thread_list, name="thread_list"),
     path("thread/<int:id>/", detailed_thread, name="detailed_thread"),
     path("thread/add/", create_thread, name="create_thread"),
-    path("thread/<int:id>/edit/", update_thread, name="update_thread")
+    path("thread/<int:id>/edit/", update_thread, name="update_thread"),
 ]
 
 app_name = "forum"
